ec344d4bc79e7dc7d0888b991fb3816fe577adab
Vitor Lourenco <vitor.registros@lourencos.net>
2021-05-26 14:24:42 -0300

    Implement user fetching from UserContext

--- src/components/MyPosts.js
+++ src/components/MyPosts.js
@@ -1,29 +1,22 @@
 import Posts from './Posts/Posts';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useContext} from 'react';
 import LayoutInterface from './LayoutInterface/LayoutInterface';
 import { getUsersPostsAsync } from '../helperFunctions/http/apiRequests';
 import {useHistory} from 'react-router-dom';
 import isValidUserState from '../helperFunctions/isValidUserState';
 import Loading from './Loading';
-
-const mockUserState = {
-  "token": "6a58d8fe-c3d4-4439-9f99-3cddf4f28430",
-  "user": {
-      "id": 7,
-      "email": "teste@teste.com",
-      "username": "teste",
-      "avatar": "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/users/1/avatar"
-  }
-}
+import UserContext from '../contexts/UserContext';
 
 export default function MyPosts(){
 
   const [posts, setPosts] = useState([]);
   const [isReadyToRender, setIsReadyToRender] = useState(false);
+  
+  const { user } = useContext(UserContext);
   const history = useHistory();
   useEffect(()=>{
-    if (!isValidUserState(mockUserState)) return;
-    const [token, id] = [mockUserState.token, mockUserState.user.id];
+    if (!isValidUserState(user)) return;
+    const [token, id] = [user.token, user.user.id];
     getUsersPostsAsync(id, token)
     .then(({data})=>{
       setPosts(data.posts);
@@ -32,7 +25,7 @@ export default function MyPosts(){
     .finally(() => setIsReadyToRender(true))
   },[])
 
-  if (!isValidUserState(mockUserState)){
+  if (!isValidUserState(user)){
     history.push("/")
   }
   
--- src/helperFunctions/isValidUserState.js
+++ src/helperFunctions/isValidUserState.js
@@ -1,6 +1,7 @@
 export default function isValidUserState(user){
   if (
-    typeof user.token === "string"
+    typeof user === "object"
+    && typeof user.token === "string"
     && typeof user.user === "object"
     && typeof user.user.id === "number"
     && typeof user.user.email === "string"
