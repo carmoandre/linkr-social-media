93cc8255827d628e14939af2db77e95d46ae8c97
Vitor Lourenco <vitor.registros@lourencos.net>
2021-05-27 22:33:30 -0300

    Add function that generates tooltips

--- src/components/Posts/Post/Likes.js
+++ src/components/Posts/Post/Likes.js
@@ -5,23 +5,25 @@ import {useState} from 'react';
 import {likePostAsync, dislikePostAsync} from '../../../helperFunctions/http/apiRequests';
 
 export default function Likes({likesProps}){
-  const { user } = likesProps;
+  const { user , posts, setPosts} = likesProps;
 
   const [likes, setLikes] = useState(likesProps.likes);
   const [isLiked, setIsLikeD] = useState(isLikedByCurrentUser(likes, user));
   
-  const toggleLike = () => toggleLikeAsync(likesProps, isLiked, setIsLikeD, setLikes);
+  const toggleLike = () => toggleLikeAsync(likesProps, isLiked, setIsLikeD, likes, setLikes, posts, setPosts);
+  
+  const tooltip = getTooltip(likes, user);
 
   return (
     <>
       {isLiked 
         ? <>
-            <IoHeartSharp onClick={toggleLike} color="red" size="20" data-tip="banana" />
+            <IoHeartSharp onClick={toggleLike} color="red" size="20" data-tip={tooltip} />
             <ReactTooltip  place="bottom" type="light" effect="solid" />
           </>
         : 
           <>
-            <IoHeartOutline onClick={toggleLike} color="white" size="20" data-tip="batata" />
+            <IoHeartOutline onClick={toggleLike} color="white" size="20" data-tip={tooltip} />
             <ReactTooltip  place="bottom" type="light" effect="solid" />
           </>
       }
@@ -31,14 +33,34 @@ export default function Likes({likesProps}){
   );
 }
 
+function getTooltip(likes, user){
+  const whoLikedList = likes.map((like) => like["user.username"] || like.username);
 
+  const orderedWhoLikedList = !isLikedByCurrentUser(likes, user)
+    ? whoLikedList
+    : ["Você", ...whoLikedList.filter(username=>username !== user.user.username)];
+
+  let tooltip = "";
+  const person1 = orderedWhoLikedList[0];
+  const person2 = orderedWhoLikedList[1];
+  const numberOfotherPeople = orderedWhoLikedList.slice(2,).length;
+
+  if (numberOfotherPeople > 0){
+    tooltip = `${person1}, ${person2} e outras ${numberOfotherPeople} pessoas`;
+  } else {
+    tooltip += person1 ? person1 : "";
+    tooltip += person2 ? ` e ${person2}` : "";
+  }
+
+  return tooltip;
+}
 
 function isLikedByCurrentUser(likes, user){
   const idsOfLikes = likes.map(like=>like["user.id"]);
   return idsOfLikes.includes(user.user.id);
 }
 
-function toggleLikeAsync(likesProps , isLiked, setIsLikeD, setLikes){
+function toggleLikeAsync(likesProps , isLiked, setIsLikeD, likes, setLikes, posts, setPosts){
   const { user, postID } = likesProps;
   const request = isLiked 
     ? dislikePostAsync(postID, user.token) 
@@ -46,6 +68,7 @@ function toggleLikeAsync(likesProps , isLiked, setIsLikeD, setLikes){
 
   request.then(response => {
     setIsLikeD(!isLiked);
-    setLikes(response.data.post.likes);
+    setLikes([...likes]);
+    setPosts([...posts]);
   });
 }
